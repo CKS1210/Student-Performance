31080d720c814765fcf64a5aceaaca3d1edb3e63
CKS1210 <119333712+CKS1210@users.noreply.github.com>
2023-12-20 15:58:15 +0800

    Adding Hyperparameter Tuning File

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -37,13 +37,37 @@ class ModelTrainer:
                 "Decision Tree" : DecisionTreeRegressor(),
                 "Gradient Boosting" : GradientBoostingRegressor(),
                 "Linear Regression" : LinearRegression(),
-                "K-Neighbors Classifier" : KNeighborsRegressor(),
-                "XGBClassifier" : XGBRegressor(),
-                "CatBoosting Classifier" : CatBoostRegressor(verbose=False),
-                "AdaBoost Classifier" : AdaBoostRegressor()
+                'K-Nearest Regressor' : KNeighborsRegressor(),
+                "XGBRegressor" : XGBRegressor(),
+                "CatBoosting Regressor" : CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor" : AdaBoostRegressor()
             }
 
-            model_report : dict = evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            params = {
+                "Random Forest" : {'n_estimators' : [8,16,32,64,128,256]},
+
+                "Decision Tree" : {'criterion' : ['squared_error', 'friedman_mse', 'absolute_error', 'poisson']},
+
+                "Gradient Boosting" : {'learning_rate' : [.1, .01, .05, .001],
+                                       'subsample' : [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                                       'n_estimators' : [8,16,32,64,128,256]},
+
+                "Linear Regression" : {},
+
+                'K-Nearest Regressor' : {'n_neighbors' : [5, 7, 9, 11]},
+
+                "XGBRegressor" : {'learning_rate' : [.1, 0.01, 0.05, 0.001],
+                                   'n_estimators' : [8, 16, 32, 64, 128, 256]},
+
+                "CatBoosting Regressor" : {'depth' : [6,8,10],
+                                            'learning_rate' : [0.01, 0.05, 0.1],
+                                            'iterations' : [30, 50, 100]},
+
+                "AdaBoost Regressor" : {'learning_rate' : [.1, .01, 0.5, .001],
+                                         'n_estimators' : [8, 16, 32, 64, 128, 256]}
+            }
+
+            model_report : dict = evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, param = params)
             
             # to get best model score from dict
             best_model_score = max(sorted(model_report.values()))
@@ -52,6 +76,7 @@ class ModelTrainer:
             best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
 
             best_model = models[best_model_name]
+            #best_model_params = best_model.params
 
             if best_model_score <0.6:
                 raise CustomException("No best model found")
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ import pandas as pd
 import pickle
 from sklearn.metrics import r2_score
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
+from src.logger import logging
 
 
 def save_object(file_path, obj):
@@ -17,13 +19,23 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            # gridsearch cv 
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            # logging.info(f"Best params: {gs.best_params_}")
             model.fit(X_train, y_train)
+
+            # model.fit(X_train, y_train)
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
